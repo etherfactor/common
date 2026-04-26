ef7d7f1ac52c7a775d6c91909e3eb495705c243d
Ryan Duffy <ether.factor@gmail.com>
2026-04-26 12:07:07 -0500

    Some more tests

---
+++ src/Notifications/tests/Notifications.IntegrationTests/Setup.cs
@@ -0,0 +1,46 @@
+﻿using Npgsql;
+using Testcontainers.PostgreSql;
+
+namespace EtherGizmos.Common;
+
+[SetUpFixture]
+internal static class Setup
+{
+    public static string PgSqlConnectionString { get; private set; }
+
+    [OneTimeSetUp]
+    public static async Task OneTimeSetUp()
+    {
+        try
+        {
+            var pgSql = new PostgreSqlBuilder("postgres:18")
+                .Build();
+
+            await pgSql.StartAsync();
+
+            PgSqlConnectionString = pgSql.GetConnectionString();
+        }
+        catch (Exception ex)
+        {
+            Assert.Ignore(ex.Message);
+        }
+    }
+
+    public static async Task<string> CreateDatabase(
+        string database)
+    {
+        using var connection = new NpgsqlConnection(PgSqlConnectionString);
+        await connection.OpenAsync();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = $"create database {database}";
+        await command.ExecuteNonQueryAsync();
+
+        var builder = new NpgsqlConnectionStringBuilder(PgSqlConnectionString)
+        {
+            Database = database,
+        };
+
+        return builder.ToString();
+    }
+}
