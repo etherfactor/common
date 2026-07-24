85f792a36b05a4bfaae91371fd25c51fc8e45bcd
Ryan Duffy <ether.factor@gmail.com>
2026-07-24 16:57:26 -0500

    Add OpenTelemetry support to unit of work

---
+++ src/Data/src/Data.UnitOfWork/UnitOfWorkActivitySourceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace EtherGizmos.Common;
+
+public static class UnitOfWorkActivitySourceExtensions
+{
+    private static ActivitySource Source { get; } = new("EtherGizmos.Common.Data.UnitOfWork");
+
+    extension(ActivitySources)
+    {
+        public static ActivitySource UnitOfWork => Source;
+    }
+}
